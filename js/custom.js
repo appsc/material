996e166182942646672272414a42172f8185b717
suribabu <suri.babu988@gmail.com>
2019-11-01 15:19:48 +0530

    wip

--- js/custom.js
+++ js/custom.js
@@ -10,17 +10,69 @@
 
         //json data get
         $.getJSON("js/meterials.json", function (data) {
-            //  let main_key;
-            //  let sub_key;
-            //materials display logic
-            $.each(data.group1.history.oldhistory, function (key, val) {
-                let materials = $('<div class="col-md-3">' + val.chapter1 + '</div>')
-                $('#examList').append(materials);
+            let main_keys;
+            let exam_key;
+            
+
+            $.each(data.exams, function (key, val) {
+                exam_key = key;
+                let exam_name = exam_key.replace("_", " ");
+                let exams_list = $('<li class="nav-item">'+
+                    '<a class="nav-link examname active" data-toggle="pill" data-exam="'+exam_key+'" href="#'+exam_key+'">'+ exam_name +'</a>'+
+                    '</li>');
+                $('#exam-pills').append(exams_list);
+
+                      let subj_name
+                    let subj_key
+                        for (let subj in val) {
+                           subj_name = val[subj].subject_name;
+                           subj_key = val[subj].key;
+                        
+
+                let materials = $('<div id="'+exam_key+'" class="container tab-pane '+exam_key+'"><br>' +
+                    '<a class="subject-name" data-subject="'+subj_key+'" href="#'+subj_key+'">' + subj_name + '</a>'+
+                            '</div>' +
+                            '</div>');
+                        $('#exam-content').append(materials);
+                        $('.group_1').addClass('active');
+
+                    }
+
+
+                $('[data-exam]').on('click', function () {
+                    present_exam = $(this).data("exam");
+                    if (key == present_exam) {
+                        $('.container.tab-pane').removeClass('active');
+                        $('.'+present_exam).addClass('active');
+                    }
+                        });
+
+                        
+                });
+
+            // Testing code..
+            let subject_k;
+            $.each(data, function (k, v) {
+                $('[data-subject]').on("click", function () {
+                    curSubj = $(this).data("subject");
+                    subject_k = k;
+                    if (curSubj == subject_k) {
+                        for (let items in v.units) {
+
+                            //console.log(v.units[items].chapter_title);
+                            let lesson_detais = $('<div id="' + curSubj + '" class="subj-content ' + curSubj + ' dis-none"><h3>' + v.units[items].chapter_title + '</h3><br/><div>' + v.units[items].lesson_content + '</div></div>');
+                            $('.display_lesson').append(lesson_detais)
+                        }
+
+                        $('.subj-content').removeClass('dis-block').addClass('dis-none');
+                        $('.' + curSubj).addClass('dis-block').removeClass('dis-none');
+                    }
+
+                });
 
             });
 
-        });       
 
-
-
-    });
+        });    
+      
+});
